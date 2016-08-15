8e4996e0f2fa67c5955a0247222961f8f542df8c
Shai Davis <shaidavis@MBP.local>
2016-08-15 14:53:27 +0300

    add barrel roll branch created

--- jets.js
+++ jets.js
@@ -2,14 +2,17 @@ var Jet = function (color, size) {
   this.color = color;
   this.size = size;
 
-  this.takeOff = function () {
-    console.log('lift off!');
-
-  }
-
   this.fly = function () {
     console.log('whoosh!');
+  };
 
+  this.takeOff = function () {
+    console.log('lift off!');
+  };
+
+  this.barrelRoll = function () {
+    console.log('rolling!');
+  };
 }
 
 module.exports = Jet;
